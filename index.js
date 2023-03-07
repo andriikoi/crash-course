e31c2a3a72acc5b5843bb0936b612b6028247a25
Andrii Koitechs <andrii.koitechs@gmail.com>
2023-03-07 14:49:15 +0200

    add dotenv

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
 const app = express();
-const port = 3000;
+
+dotenv.config();
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`Crash course app listening on port ${port}`)
